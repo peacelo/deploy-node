f468965ce13c9c9db4274e01b68ed518a67c3238
Marcelo <niqcelo@gmail.com>
2020-10-29 15:49:15 -0300

    add-master

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.use(cors('*'));
+app.use(express.json())
+
 app.get('/', (req, res)=>{
-    res.send('hello,qwdwdqdqdqwd world !');
+    fetch(url, {
+        
+        method: 'POST',
+        body: {
+            description: "My first webhook",
+            callbackURL: "https://deploy-example123.herokuapp.com/api",
+            idModel: "RLIihjSy"
+        },
+        mode:"no-cors",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    }).then(res => console.log(res.json())).then(data => console.log(data)).catch(err => console.log(err))
+
+});
+
+app.get('/api', (req, res)=>{
+    const data = req.body;
+    res.json({data})
 });
 
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log('servidor rodfwewfwefando ! ')
+    console.log('servidor rodfwedqwdwqwfwefando!')
 })
