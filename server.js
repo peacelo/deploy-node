9e53904dd1ea0b8e700f5f7529de78ad2981a2b3
Marcelo Filho <niqcelo@do.ma.gov.br>
2020-10-29 14:02:58 -0300

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.send('hello,qwdwdqdqdqwd world !');
-})
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('servidor rodfwewfwefando ! ')
