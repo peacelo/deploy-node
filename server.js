1b07d364ae074f9271486efc3999a6785169da43
Marcelo <niqcelo@gmail.com>
2020-11-04 10:04:08 -0300

    changes

--- server.js
+++ server.js
@@ -6,18 +6,14 @@ const app = express();
 app.use(express.json());
 app.use(cors('*'));
 
-app.get('/',(req, res)=>{
-    
-        res.send(req.body)
 
-});
+
+
 app.post('/', (req, res)=>{
-    console.log(req.body);
+    res.json(req.body)
 })
 
-app.get('/api', (req, res)=>{
-    res.send(req.body);
-});
+
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('servidor rodfwedqwdwqwfwefando!')
