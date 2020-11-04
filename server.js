4949ed44ef3e5adf05904135d7d4a4de0c3e4b84
Marcelo Penha <niqcelo@gmail.com>
2020-11-04 20:54:36 -0300

    big changes

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
-const app = express();
 
 app.use(express.json());
 app.use(cors('*'));
+
+
+const getApiSocket = socket =>{
+    const response = new Date();
+    
+
+}
+
+server.listen(process.env.PORT || 3000, ()=>{
+    console.log('servidor rodando!')
+})
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,13 +30,16 @@ app.use(bodyParser.json());
 app.post('/', (req, res)=>{
     console.log(req.body.action);
     res.send(req.body);
-    console.log('hello')
+    console.log('testando API');
+
+    io.on('connection', socket=>{
+        console.log('new socket');
+        
+        socket.emit('FromApi', req.body.action);
+    })
 })
 
 app.get('/', (req, res)=>{
 res.send('Hello word pola')
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('servidor rodfwedqwdwqwfwefando!')
-})
