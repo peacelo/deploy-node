7dcb3cbdb6efdc924d5e599b9e6ec35a7cf238fb
Marcelo <niqcelo@gmail.com>
2020-11-04 08:59:01 -0300

    changes

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cors('*'));
 
-
-const APIKey = '02b8303f3c296818e1d5f30b3dd944db'
-const APIToken = 'f0a48555f471168e3ed7d553ac0462c834b947fa2ac86c28f0f744b5f5e683bb';
-
-const url = `https://api.trello.com/1/tokens/${APIToken}/webhooks/?key=${APIKey}`;
-
 app.get('/', async (req, res)=>{
+    const APIKey = '02b8303f3c296818e1d5f30b3dd944db'
+    const APIToken = 'f0a48555f471168e3ed7d553ac0462c834b947fa2ac86c28f0f744b5f5e683bb';
+    
+    const url = `https://api.trello.com/1/tokens/${APIToken}/webhooks/?key=${APIKey}`;
 
         const body = {
             "description": "My first webhook",
