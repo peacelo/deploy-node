83c158ba16ba72b51dc9cf949232e93e811cb112
Marcelo <niqcelo@gmail.com>
2020-11-04 09:35:12 -0300

    changes

--- server.js
+++ server.js
@@ -6,32 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cors('*'));
 
-app.get('/', async (req, res)=>{
-    const APIKey = '02b8303f3c296818e1d5f30b3dd944db'
-    const APIToken = 'f0a48555f471168e3ed7d553ac0462c834b947fa2ac86c28f0f744b5f5e683bb';
-    
-    const url = `https://api.trello.com/1/tokens/${APIToken}/webhooks/?key=${APIKey}`;
 
-        const body = {
-            "description": "My first webhook",
-            "callbackURL": "https://deploy-example123.herokuapp.com/api",
-            "idModel": "RLIihjSy"
-        }
-    
-        const response = await fetch(url, {
-            method: 'post',
-            body: JSON.stringify(body),
-            headers: {'Content-Type': 'application/json'}
-        });
-        const json = await response.json();
-    
-        res.send(json)
+app.get('/', (req, res)=>{
+    res.send(req);
+})
 
-});
-
-app.get('/api', (req, res)=>{
-    res.send(req.body);
-});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('servidor rodfwedqwdwqwfwefando!')
